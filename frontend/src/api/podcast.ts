d0928b485ff402d00c834726e825f962bbbc0f41
code-ganesh <ganesh.cs22@sahyadri.edu.in>
2025-08-19 00:37:22 +0530

    backend done"

--- frontend/src/api/podcast.ts
+++
@@ -1,8 +0,0 @@
-import { api } from "./client";
-import type { PodcastResponse } from "./types";
-import { DEFAULT_PERSONA, DEFAULT_TASK } from "../config";
-
-export async function createPodcast(section_texts: string[], persona=DEFAULT_PERSONA, task=DEFAULT_TASK) {
-  const { data } = await api.post<PodcastResponse>("/podcast", { section_texts, persona, task });
-  return data;
-}
